27d220199f9351c1ed6fff8dc4204827069b63d0
LorenaForero <77217186+LorenaForero@users.noreply.github.com>
2021-02-13 12:59:45 -0500

    Creación proyecto inicial base

---
+++ src/main/java/com/tienda/manualidades/TiendaManualidadesApplication.java
@@ -0,0 +1,13 @@
+package com.tienda.manualidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaManualidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaManualidadesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tienda/manualidades/TiendaManualidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.manualidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaManualidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
